eac6320d28f209ac8fc5d97cbd5982d3109a84ea
Christian Olwitz <Christian.Olwitz@outlook.de>
2022-09-10 19:53:52 +0200

    gvhjuzf

---
+++ BFW_Semester_2/Aufgabe_24.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BFW_Semester_2
+{
+    internal class Aufgabe_24
+    {
+        public static void Start()
+        {
+            string classroom = "+-------+-------------+------+\n|       +-------------+      |\n|                            |\n|                            |\n" +
+                "|  +----+   +----+   +----+  |\n|  |    |   |    |   |    |  |\n|  +----+   +----+   +----+  |\n|                            |\n|                            |\n" +
+                "|  +----+   +----+   +----+  |\n|  |    |   |    |   |    |  |\n|  +----+   +----+   +----+ -+\n|\n|\n|  +----+   +----+   +----+ -+\n|  |    |   |    |   |    |  |\n" +
+                "|  +----+   +----+   +----+  |\n|                            |\n+----------------------------+";
+            Console.WriteLine(classroom);
+            string er = Console.ReadLine();
+        }
+    }
+}
