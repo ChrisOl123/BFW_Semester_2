fe1ab4a4bbbe5cdfed2f981a8a0dfce088e657ea
Christian Olwitz <Christian.Olwitz@wibizu.de>
2022-11-14 16:52:09 +0100

    Nicht fertig

---
+++ BFW_Semester_2/Aufgabe_17.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace BFW_Semester_2
+{
+    class Aufgabe_17
+    {                                         // Pfad @"U:\Csharp\Aufgabe17.txt"
+        static List<Tuple<string, List<string>>> mainList = new List<Tuple<string, List<string>>>();
+        public static void Start()
+        {
+            Menu();
+        }
+        static void Input()
+        {
+
+        }
+        static void Menu()
+        {
+            Console.Clear();
+            Console.WriteLine("Welche Liste möchten Sie bearbeiten?");
+            LoadList();
+        }
+        static void LoadList()
+        {
+            List<string> load = new List<string>();
+            Tuple<string, List<string>> tuple;
+            if (File.Exists(@"U:\Csharp\Aufgabe17.txt"))
+            {
+                foreach (string line in File.ReadAllLines(@"U:\Csharp\Aufgabe17.txt"))
+                {
+                    load.Add(line);
+                    tuple = new Tuple<string, List<string>>();
+                    Console.WriteLine(mainList);
+                }
+            }
+            else
+                File.WriteAllText(@"U:\Csharp\Aufgabe17.txt", " ");
+
+        }
+        static void SaveList()
+        { 
+
+        }
+
+    }
+}
--- BFW_Semester_2/Program.cs
+++ BFW_Semester_2/Program.cs
@@ -2,13 +2,14 @@
 using System.Collections.Generic;
 using System.Text;
 
+
 namespace BFW_Semester_2
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Aufgabe_16.Start();
+            Aufgabe_17.Start();
         }
     }
 }
